849bfd44333b6cf4f62d72cba3dcabcaaa667571
PJaramilloV <pablojaramillovesperinas@gmail.com>
2022-05-18 17:22:24 -0400

    camara mejorada

--- scripts/objeto.gd
+++ scripts/objeto.gd
@@ -14,9 +14,9 @@ func take_damage(instigator: Node):
 	
 	# el primero es para lanzar los objetos hacia adelante del personaje
 	#apply_central_impulse((self.global_transform.origin - instigator.global_transform.origin)*5)
-	var eje_z = get_viewport().get_mouse_position().x
+	var eje_x = get_viewport().get_mouse_position().x
 	var eje_y = get_viewport().get_mouse_position().y
-	var raton = Vector3(0,clamp((380-eje_y)/30, -3.5, 5),clamp(-(eje_z-500)/50, -4.3, 4.3))
+	var raton = Vector3(clamp((eje_x-500)/50, -4.3, 4.3),clamp((380-eje_y)/30, -3.5, 5),0)
 	apply_central_impulse(raton)
 
 
