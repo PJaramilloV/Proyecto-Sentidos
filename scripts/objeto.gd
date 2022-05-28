1bc733e89d9c0cc6a06badb48a5c8eeb6ccad5d8
PJaramilloV <pablojaramillovesperinas@gmail.com>
2022-05-28 19:25:04 -0400

    "air_timer" permite lanzamiento a traves del personaje  + funciones documentadas

--- scripts/objeto.gd
+++ scripts/objeto.gd
@@ -2,45 +2,80 @@ extends RigidBody
 var my_material := []
 var material_count
 onready var path = get_parent().get_node("trajectory")
-var throws_before_break = 3
+var throws_before_break = 10
 var thrown = false
+var process_function = 'sleep'
+var air_time = 0
+var air_time_limit = 0.2
 
-# get_active_material && get_surface_material
-
+# get_active_material && get_surface_materia
 func _ready():
 	material_count = find_node("MeshInstance").get_surface_material_count()
 	for i in range(material_count):
 		var mat = find_node("MeshInstance").get_surface_material(i)
 		my_material.append(mat)
 
+# Funcion nominal process_function(delta)
+func sleep(delta):
+	pass
+
+# Funcion de timer process_function(delta)
+func timer_run(delta):
+	air_time += delta
+	print(air_time)
+	if(air_time > air_time_limit):
+		self.collision_mask=2
+		process_function = 'sleep'
+		air_time = 0
+
+# Funcion a llamar en cada frame
+func _process(delta):
+	call(process_function, delta) # llamar process_function(delta)
+
+# Al ser agarrado, deactivar coll_mask
 func grab():
 	self.mode = RigidBody.MODE_KINEMATIC
 	self.collision_mask=0
+	process_function = 'sleep'
+	air_time = 0
 
+# Enciende deteccion de colisiones y activa timer
+func activate_throw():
+	self.mode = RigidBody.MODE_RIGID
+	process_function = 'timer_run'
+	self.contact_monitor = true   # Detectar siguiente colision
+	thrown = true
 
+# Apaga la deteccion de colisiones
+func deactivate_hold():
+	self.contact_monitor = false   # Detectar siguiente colision
+	thrown = false
+
+# Funcion a llamar al soltar objeto
 func release():
 	clear_path()
 	self.mode = RigidBody.MODE_RIGID
 	self.collision_mask=2
-	self.contact_monitor = false   # Detectar siguiente colision
-	thrown = false
-
+	deactivate_hold()
+	
+# Funcion a llamar al lanzar objeto
 func throw(instigator: Node):
-	release()
+	clear_path()
+	activate_throw()
 	# el primero es para lanzar los objetos hacia adelante del personaje
 	#apply_central_impulse((self.global_transform.origin - instigator.global_transform.origin)*5)
 	var eje_x = get_viewport().get_mouse_position().x
 	var eje_y = get_viewport().get_mouse_position().y
 	var raton = Vector3(0,clamp((380-eje_y)/30, -3.5, 5),-clamp((eje_x-500)/50, -4.3, 4.3))
 	apply_central_impulse(raton)
-	self.contact_monitor = true   # Detectar siguiente colision
-	thrown = true
-
-
+	
+	
+# Restorar material de meshes
 func restore():
 	for i in range(material_count):
 		get_node("MeshInstance").set_surface_material(i, my_material[i])
 
+# Aplicar outline al objeto
 func outline(material):
 	for i in range(material_count):
 		var mat = find_node("MeshInstance").get_surface_material(i)
@@ -49,6 +84,7 @@ func outline(material):
 	#var nuevomaterial = SpatialMaterial.new()
 	# se puede buscar el hijo que sea mesh con un for y era
 	
+# Calcular puntos de parabola
 func get_predicted_trajectory():
 	var delta      = 0.1   # dt entre puntos de trayectoria
 	var target_x   = get_viewport().get_mouse_position().x
@@ -68,7 +104,7 @@ func get_predicted_trajectory():
 		trajectory.append(t_point)
 	return trajectory
 	
-	
+# Graficar parabola desde la posición actual "from_position"
 func display_predicted_trajectory(from_position):
 	self.global_transform.origin = from_position
 	var trajectory_points = get_predicted_trajectory()
@@ -77,11 +113,12 @@ func display_predicted_trajectory(from_position):
 	for point in trajectory_points:
 		path.add_vertex(point)
 	path.end()
-	
+
+# Borrar parabola
 func clear_path():
 	path.clear()
 
-
+# Llamada solo cuando hay una colision monitoreada
 func on_body_collided(surface: StaticBody):
 	if(! thrown): return # Evitar colisiones dobles
 	if(surface is StaticBody): # al colisionar con superficies, ejecutar
