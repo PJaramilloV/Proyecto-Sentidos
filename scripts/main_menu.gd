488acda12b74976d16e257c63837f60646104f51
Diego <diego.torreblanca77@gmail.com>
2022-07-19 02:04:57 -0400

    agregar boton credits y retocar escena main menu
    agegar el boton para direccionar a la escena de creditos, colocar titulo del juego en un mejor lugar y retocar los botones

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,6 +5,7 @@ onready var start = $Panel/Fondo/Start
 onready var exit = $Panel/Fondo/Exit
 onready var options = $Panel/Fondo/Options
 onready var extras = $Panel/Fondo/Extras
+onready var credits = $Panel/Fondo/Credits
 onready var settings_menu = $Settings_Menu
 onready var background = $MainMenu2
 var level : int
@@ -22,6 +23,7 @@ func _ready():
 	exit.connect("pressed", self, "_on_exit_pressed")
 	options.connect("pressed", self, "_on_options_pressed")
 	extras.connect("pressed", self, "_on_extras_pressed")
+	credits.connect("pressed", self, "_on_credits_pressed")
 	load_level()
 	if level != 0:
 		continueb.visible = true
@@ -52,6 +54,11 @@ func _on_extras_pressed():
 	yield(get_tree().create_timer(time_in_seconds), "timeout")
 	#get_tree().change_scene("res://demo/DemoHero.tscn")
 	emit_signal("extra")
+
+func _on_credits_pressed():
+	$AudioStreamPlayer2D.play()
+	yield(get_tree().create_timer(time_in_seconds), "timeout")
+	get_tree().change_scene("res://godot-credits-master/GodotCredits.tscn")
 
 func load_level():
 	var save_file = File.new()
